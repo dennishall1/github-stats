70153bd823ec0b488ca3909596e9b5a6f9b007e9
Dennis Hall <hall.dennis+github@gmail.com>
2015-08-22 19:54:07 -0400

    significant rewrite.
    focus on labels
    count merges
    reconcile PRs for notifications that do not include PR#.

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,7 @@ module.exports = function (router) {
     // NOTE: router paths are based on the location of this file
     router.get('/', function (req, res) {
         res.render('index', {
-            title: "GitHub Stats",
-            body: "... later, we will show actual stats here.  For now, we're just collecting the stats"
+            title: "GitHub Stats"
         });
     });
     
